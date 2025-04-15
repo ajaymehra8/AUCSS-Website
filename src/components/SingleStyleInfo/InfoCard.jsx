971b4ebf48782668ad7db2ff454aeee673bd6c1b
Ajay Mehra <ajaymehra4492@gmail.com>
2025-04-15 18:56:06 +0530

    Add usage of toaster

--- src/components/SingleStyleInfo/InfoCard.jsx
+++ src/components/SingleStyleInfo/InfoCard.jsx
@@ -12,7 +12,7 @@ const InfoCard = ({ info }) => {
   return (
     <div>
       <h1 className="text-white font-bold text-2xl">
-        {info.title} ( {info.shortcut} ) :
+        {info.title} {info.shortcut&&`( ${info.shortcut} )`} :
       </h1>
       <p className="text-md tracking-wider mt-4">{info.description}</p>
       <pre className="relative mt-3 bg-black/30 rounded-lg p-4 text-sm text-gray-300 font-mono cursor-pointer">
